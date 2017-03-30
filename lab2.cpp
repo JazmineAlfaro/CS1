2eab1003f30dcf89252a4c83e0f417a396ad92df
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-03-30 15:47:03 -0500

    Laboratorio 2

--- lab2.cpp
+++ lab2.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 
 int main() {
+		/*
+	cout << "Ingrese un numero: "<<endl;
+	for(x=0;x<i;++x)
+		cin >> arreglo[x];
+	for(x=0;x<i;++x)
+		cout<< arreglo[x] <<endl;
+	*/
+		/*
+	int a,b,z;
+	cout << "Ingrese un numero: "<<endl;
+	cin >> a;
+	cout << "Ingrese un numero: "<<endl;
+	cin >> b;
+	if(a>b)
+		z=a;
+	else
+		z=b;
+	
+	z = (a>b)?a:b;
+	cout << "El mayor es: ">> z <<endl;
+	*/
 	int score[3];
 	int c = 0;
 	cin >> score[c++];
