a9e6cd8947cac786a9e4561b03a3d54335d5671e
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-03-30 15:44:30 -0500

    Laboratorio 2

---
+++ lab2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score[3];
+	int c = 0;
+	cin >> score[c++];
+	cin >> score[c++];
+	cin >> score[c++];
+	int temp;
+	temp = score[0];
+	score[0] = score[2];
+	score[2] = temp;
+	cout << score[0];
+	cout << score[1];
+	cout << score[2];
+	return 0;
+
+}
+
