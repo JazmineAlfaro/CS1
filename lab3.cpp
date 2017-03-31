9ca5fc522286c54f8cb3b65ae6411cf9f64e6c3b
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-03-31 11:21:22 -0500

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,42 +1,72 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="lab3" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/lab3" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/lab3" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="-fexceptions" />
-		</Compiler>
-		<Unit filename="main.cpp" />
-		<Extensions>
-			<code_completion />
-			<envvars />
-			<debugger />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    //Intercambio de posiciones en un arreglo de 4 elementos
+    int arreglo[4];
+    int c = 0;
+    cout << "Ingrese numeros para el arreglo: " << endl;
+    cin >> arreglo[0];
+    cin >> arreglo[1];
+    cin >> arreglo[2];
+    cin >> arreglo[3];
+    int temp;
+    temp = arreglo[0];
+    arreglo[0] = arreglo[3];
+    arreglo[3]= temp;
+    temp = arreglo[1];
+    arreglo[1]=arreglo[2];
+    arreglo[2]=temp;
+
+    cout << arreglo[0];
+    cout << arreglo[1];
+    cout << arreglo[2];
+    cout << arreglo[3];
+    */
+
+    /*
+    //Intercambio de posiciones en un arreglo de n elementos
+    int arreglo[10];
+    int a,c=0;
+
+
+    }
+
+    for(a = 0;a < c/2; a++){
+        int temp, fin = c-1-a;
+        temp = arreglo[a];
+        arreglo[a] = arreglo[fin];
+        arreglo[fin]= temp;
+    }
+
+    for(a = 0; a < 10; a++ ){
+        cout << arreglo[a] <<endl;
+    }
+    */
+/*
+    int arreglo[5];
+    int a,c=0;
+    for(a = 0; a < 5; a++ ){
+        cout<< "Ingrese los elementos del arreglo: " <<endl;
+        cin>>arreglo[a];
+        c+=arreglo[a];
+    }
+    cout<<c<<endl;
+    */
+
+    int a,c;
+    int arreglo[5];
+    for(a = 0; a < 5; a++ ){
+        cout<< "Ingrese los elementos del arreglo: " <<endl;
+        cin>>arreglo[a];
+
+    if(arreglo[a]>c)
+        c = arreglo[a];
+
+    }
+    cout << c << endl;
+    return 0;
+}
