85e2aa7fed7bf4c62014ae4b0122bcef0c9e28dd
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-04-07 12:55:40 -0500

    lab5.1

--- lab5.cpp
+++ lab5.cpp
@@ -1,40 +1,54 @@
-
 #include <iostream>
-#include <string>
 using namespace std;
 
-int sum(int array[],int tam) {
+int sum(const int array[], const int length){
 	int sum = 0;
-	for(int i = 0; i < tam; sum += array[i++]);
-		return sum;
-	}
-	
+	for(int i=0;i<length;sum += array[i++]);
+	return sum;
+}
 
+int promedio(const int array[],const int length){
+    int algo = sum(array, length);
+	return float(algo/length);
+
+}
+
+void intercambio(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+void invertir(int array[],int length){
+    for(int i = 0;i < length/2; i++)
+        intercambio(array[i],array[length-i-1]);
+    }
+
+/*
+
+void ordenar(const int array[],const int length){
+    for(int i=0;i<length;i++){
+
+//crear una funcion  para saber el tamaño de una cadena
+//multiplicacion de matrices
+
+    }
+
+}
+*/
 int main() {
-	//ingresar elementos
-	int arreglo[5];
-	cout<<"Ingrese elementos para el arreglo" <<endl;
-	for(int i=0;i<5;i++)
-		cin>>arreglo[i];
-	/*
-	//suma
-	int cont = 0;
-	for(int i=0;i<5;i++)
-		cont += arreglo[i];
-
-	//imprimir invertido
-	for(int i=1;i<6;i++)
-		cout<< arreglo[5-i];
-	*/
-	//ordenar 
-	int a=0;
-	for(int i=0;i<5;i++)
-		if(arreglo[i]>a)
-			a = arreglo[i];
-	cout <<a;	
-	
-	//cout << "La suma de los elementos del arreglo es : " << cont << endl;
-	//cout << "El promedio de los elementos del arreglo es : " << cont/float(5)<< endl;
-	
+	int n;
+	cout << "Ingrese el tamaño del arreglo: "<<endl;
+	cin>>n;
+	int array[n];
+	cout << "Ingrese los elementos para el arreglo: "<<endl;
+	for(int i =0;i<n;i++)
+		cin>>array[i];
+	cout<<"Suma: "<<sum(array,n)<<endl;
+	cout<<"Promedio: "<<promedio(array,n)<<endl;
+	invertir(array,n);
+	cout<<"Arreglo invertido: ";
+	for(int a = 0; a < n; a++ )
+		cout << array[a] ;
 	return 0;
 }
