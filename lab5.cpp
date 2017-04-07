bd626c1dc21750f3092e80fd7b898bbd0cccb038
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-04-07 11:26:21 -0500

    Lab5

---
+++ lab5.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int sum(int array[],int tam) {
+	int sum = 0;
+	for(int i = 0; i < tam; sum += array[i++]);
+		return sum;
+	}
+	
+
+int main() {
+	//ingresar elementos
+	int arreglo[5];
+	cout<<"Ingrese elementos para el arreglo" <<endl;
+	for(int i=0;i<5;i++)
+		cin>>arreglo[i];
+	/*
+	//suma
+	int cont = 0;
+	for(int i=0;i<5;i++)
+		cont += arreglo[i];
+
+	//imprimir invertido
+	for(int i=1;i<6;i++)
+		cout<< arreglo[5-i];
+	*/
+	//ordenar 
+	int a=0;
+	for(int i=0;i<5;i++)
+		if(arreglo[i]>a)
+			a = arreglo[i];
+	cout <<a;	
+	
+	//cout << "La suma de los elementos del arreglo es : " << cont << endl;
+	//cout << "El promedio de los elementos del arreglo es : " << cont/float(5)<< endl;
+	
+	return 0;
+}
