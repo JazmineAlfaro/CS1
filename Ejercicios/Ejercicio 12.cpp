819e9688bd45391210f2e06e8bf3f73281ac5d46
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-04-20 15:58:16 -0500

    Add files via upload

---
+++ Ejercicios/Ejercicio 12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void reversa(int array[], int tam) {
+	int temp;
+	for(int i=0; i<=(tam/2); i++){
+		temp = array[i];
+		array[i] = array[tam-i-1];
+		array[tam-i-1] = temp; 
+	}
+}
+
+
+int main(){
+	int arreglo[] = {1,2,3,4,5,6,7,8,9};
+	reversa(arreglo,9);
+	for(int a=0;a<9;a++)
+		cout <<arreglo[a];
+
+	return 0;
+}
+
---
+++ Ejercicios/Ejercicio 8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int *a, int *b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	cout << *a << *b; 
+}
+
+int main() {
+	int x= 15;
+	int y = 20;
+	reverse(&x,&y);
+	
+	return 0;
+}
+
