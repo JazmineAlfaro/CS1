ac0242aa5d6694af3dc13e4fec98c775693e4c13
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-04-21 00:33:57 -0500

    Palindrome

---
+++ Ejercicios/Ejercicio15.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool palindrome(char* inicio, char* fin){
+	// inicio es un puntero a la primera posicion
+	// fin es un puntero a la ultimo posicion
+	while(inicio < fin){
+		if(*inicio != *fin)
+			return false;
+		inicio++;
+		fin--;
+	}
+	return true;
+}	
+	
+int tam(char* palabra){
+	int i = 0;
+	while(*palabra!='\0'){
+		i++;
+		*palabra++;
+	}
+	return i;
+}
+
+int main() {
+	char* algo = "aja";
+	int len = tam(algo)-1;
+	if(palindrome(algo,(algo+len)))
+		cout<< "Yay";
+	else
+		cout << "Nay";
+
+	return 0;
+}
+
