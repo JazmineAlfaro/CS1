e7c5925b0da23267353dd05b0067864c302bbc36
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-06-14 17:29:14 -0500

    Avion

--- avioncito.cpp
+++ avioncito.cpp
@@ -8,40 +8,49 @@ class Avion{
 public:
 	string modelo;
 	Avion(){modelo = " ";}
-	virtual string mensaje();
+	virtual void mensaje()=0;
 };
 
 class Torre{
 public:
 	string nombre;
-	Torre(){nombre = "  ";};
+	Torre(){
+		nombre = "  ";
+	}
 	vector<Avion*> aviones;
 	void agrega(Avion *cito){
 		aviones.push_back(cito);
 	}
-	
+	void llamando(){
+	for(int i=0;i<aviones.size();i++){
+		Avion *a = aviones[i];
+		a->mensaje();
+	}
+	}
 };
 
 class Helicoptero : public Avion{
 public:
 	double numero;
 	Helicoptero(){numero = 0;}
-	string mensaje(){return "Espera";}
+	void mensaje(){cout<< "Helicoptero: Espere"<<endl;}
 };
 
 class Jet : public Avion{
 public:
 	string piloto;
 	Jet(){piloto = " ";}
-	string mensaje(){return "Despega";}
+	void mensaje(){cout<< "Jet: Despegue"<<endl;}
 	
 };
 int main()
 {
-	Torre t();
-	//Avion a = new Avion
+	Torre t;
 	Helicoptero *h = new Helicoptero();
+	Jet *j = new Jet();
 	t.agrega(h);
-	
+	t.agrega(j);
+	t.llamando();
+
 	return 0;
 }
