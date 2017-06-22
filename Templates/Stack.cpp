72b1cdf9d656073f97fcd08f0aac85cc43597828
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2017-06-22 11:58:12 -0500

    Stack

--- Templates/Stack.cpp
+++ Templates/Stack.cpp
@@ -16,7 +16,7 @@ template <class T> Stack<T> operator+(const Stack<T> &s1,const Stack<T> &s2){
 
 template <class T> Stack<T> operator-(const Stack<T> &s1,const Stack<T> &s2){
 	Stack<T> result;
-
+	
 	int tam=s1.pila.size();
 	for(int i=0;i<tam;++i){
 		bool p=true;
@@ -24,7 +24,7 @@ template <class T> Stack<T> operator-(const Stack<T> &s1,const Stack<T> &s2){
 			if(s1.pila[i]==s2.pila[a]){
 				p=false;
 				break;
-		}
+			}
 		}
 		if(p==true){
 			result.push(s1.pila[i]);
@@ -32,24 +32,21 @@ template <class T> Stack<T> operator-(const Stack<T> &s1,const Stack<T> &s2){
 	}
 	return result ;
 }
-/*
-template <class T> &ostream operator-(const Stack<T> &s){
-	cout<<"Elementos del Stack: "<<endl;
-	cout<< "-----"<<endl;
-	for(int i=pila.size()-1;i>=0;i--){
-		cout<<"| " <<pila[i]<< " |"<<endl;
-		cout<< "-----"<<endl;
+
+template<class T> ostream& operator<<(ostream &output, const Stack<T> &s)
+{
+	for(int i=s.pila.size()-1;i>=0;i--){
+		output <<s.pila[i] << endl;
 	}
-	
-	
-}*/
+	return output;
+}
+
 template<class T> class Stack{
 private:
 	vector<T> pila;
 	friend Stack<T> operator+ <>( const Stack<T> &s1, const Stack<T> &s2);
 	friend Stack<T> operator- <>( const Stack<T> &s1, const Stack<T> &s2);
-	//friend Stack<T> operator<< <>( const Stack<T> &s1);
-	
+	friend ostream& operator<< <>(ostream &output, const Stack<T> &s);
 public:
 	void push(const T &algo){
 		pila.push_back(algo);
@@ -93,13 +90,13 @@ int main(){
 	Pila.push(8);
 	Pila.push(6);
 	Pila.push(5);
-	
+	cout<<Enteros;
 	
 	Stack<int> Res = Enteros - Pila; 
-	Res.print();
+	cout<<Res;
+	//Res.print();
 	//cout<<Enteros.top()<<endl;
 	//Enteros.print();
 	//Enteros.pop();
 	
 	return 0;
-}
